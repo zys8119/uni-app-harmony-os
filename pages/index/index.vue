2bfc02092932c79dfa766b952fd61c7c514193aa
zys8119 <770959294@qq.com>
2025-09-23 13:57:17 +0800

    feat: 初始化uni-app项目并添加NFC功能模块
    添加基础项目文件包括App.vue、main.js、pages.json等
    实现NFC读写功能模块js_sdk/hexiii-nfc
    添加静态资源logo.png
    配置.gitignore忽略unpackage目录

---
+++ pages/index/index.vue
@@ -0,0 +1,71 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<button class="button" @click="openAppProductBtn">
+				asdas-{{ options }}
+			</button>
+		</view>
+		<view>
+			<embed class="native-button" tag="button" :options="options" @buttonclick="onClick"></embed>
+		</view>
+	</view>
+</template>
+
+<script lang="uts">
+export default {
+	data() {
+		return {
+			options:{}
+		}
+	},
+	onShow() {
+		console.log(uni.openAppProduct())
+	},
+	methods: {
+		async openAppProductBtn() {
+		},
+		onClick(e) {
+			console.log('onClick', e.detail.text)
+			this.options = {
+				label: 'world'
+			}
+		}
+	}
+}
+</script>
+
+<style>
+.native-button {
+	display: block;
+	width: 200px;
+	height: 50px;
+	margin: 10px auto;
+}
+
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+
+.logo {
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
+}
+
+.text-area {
+	display: flex;
+	justify-content: center;
+}
+
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
+</style>
